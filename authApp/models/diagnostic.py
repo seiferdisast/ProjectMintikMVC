67879ba7bcaca46db75fc5d5e8390e4b2af02bcc
Darly145 <darlyrodriguez75@gmail.com>
2022-09-12 20:24:13 -0500

    Diagnostic class was created

--- authApp/models/diagnostic.py
+++ authApp/models/diagnostic.py
@@ -2,7 +2,7 @@ from django.db import models
 from .user import user
 
 class Diagnostic(models.Model):
- diagnosticID = models.Intfield('diagnosticID',primary_key = True)
- diagnostic = models.CharField(max_length=255)
+ diagnosticID = models.IntegerField(default = 10)
+ patient_diagnostic = models.CharField(default = 255)
  date = models.DateTimeField()
- users_documentID = models.Charfield('users_documentID',foreing_key = True)
+ users_documentID = models.CharField(default = 10)
