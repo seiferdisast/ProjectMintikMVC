38670824ebf9f634fa3d36c1c2e742f5d597ae37
seiferdisast <seifer.code@gmail.com>
2022-09-13 12:48:33 -0500

    Modified atributes assings

--- authApp/models/user.py
+++ authApp/models/user.py
@@ -4,7 +4,6 @@ from django.contrib.auth.hashers import make_password
 
 class UserManager(BaseUserManager):
     
-    
         def createUser(self, documentId, password = None):
             if not documentId:
                 raise ValueError("Users must have an documentId ")
@@ -22,24 +21,24 @@ class UserManager(BaseUserManager):
             user.is_admin = True
             user.save(using = self._db)
             return user
-        
-class user(AbstractBaseUser, PermissionsMixin):
-    documentId = models.Charfield('DocumentId',primary_key = True)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    documentId = models.Charfield('DocumentId', primary_key = True)
     name = models.Charfield(45)
     lastName = models.Charfield(45)
     cellularPhone = models.Charfield(10)
     address = models.Charfield(45)
     email = models.Charfield(45)
     role = models.Charfield(20)
-    password = models.CharField('Password' ,max_length = 8)
-    assignDoctor = models.Charfield(45)
-    assignNurse = models.Charfield(45)
-    assignRelative = models.Charfield(45)
+    password = models.CharField('Password' , max_length = 8)
+    assignDoctor = models.ForeignKey('self', on_delete = models.SET_NULL)
+    assignNurse = models.ForeignKey('self', on_delete = models.SET_NULL)
+    assignRelative = models.ForeignKey('self', on_delete = models.SET_NULL)
 
     def save(self,  **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
         self.password = make_password(self.password, some_salt)
         super().save(**kwargs)
     
-    objects = UserNabager()
+    objects = UserManager()
     USERNAME_FIELD = 'documentId'
